c5c6a10767a4a64fad333e4ed091553febe26e1a
freeohariki1998 <freeohariki1998@gmail.com>
2026-04-19 22:14:35 +0900

    SQLをid順に修正

--- ai-api/src/db/candles_odl.ts
+++ ai-api/src/db/candles_odl.ts
@@ -4,9 +4,9 @@ import { OldLog } from "../types/oldLog";
 export async function getCandlesOld(): Promise<OldLog[]> {
   const [rows] = await db.query(
     `
-    SELECT code, action, side, price, quantity, trade_datetime, strategy_name, is_sim
+    SELECT *
     FROM trade_logs_old
-    ORDER BY code ASC, trade_datetime ASC
+    ORDER BY id
   `
   );
 
